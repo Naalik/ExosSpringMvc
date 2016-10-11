79f5d307d348bcc94fc1c09f85904a9d412fced1
Naalik <lormir.panther@gmail.com>
2016-10-11 13:42:02 +0200

    Commit exo site springmvc java bdd

--- src/main/java/streaming/controller/FilmController.java
+++ src/main/java/streaming/controller/FilmController.java
@@ -13,11 +13,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import streaming.entity.Film;
-import streaming.entity.Genre;
 import streaming.service.FilmCrudService;
-import streaming.service.GenreCrudService;
 
 /**
  *
@@ -25,39 +22,44 @@ import streaming.service.GenreCrudService;
  */
 @Controller
 public class FilmController {
+
     @Autowired
     private FilmCrudService crud;
+
     @RequestMapping(value = {"/liste_film", "/"}, method = RequestMethod.GET)
-    public String lister(Model m){
+    public String lister(Model m) {
         List<Film> films = crud.findAllBy();
         m.addAttribute("films", films);
         return "film_lister.jsp";
     }
+
     @RequestMapping(value = "/supprimer_film/{val}", method = RequestMethod.GET)
-    public String supprimer(@PathVariable("val") Long id){
+    public String supprimer(@PathVariable("val") Long id) {
         crud.delete(id);
         return "redirect:/liste_film";
     }
+
     @RequestMapping(value = "/editer_film/{val}", method = RequestMethod.GET)
-    public String editer(@PathVariable("val") Long id, Model m){
+    public String editer(@PathVariable("val") Long id, Model m) {
         Film film = crud.findOneById(id);
         m.addAttribute("film", film);
         return "modifier_film.jsp";
     }
+
     @RequestMapping(value = "/editer_film", method = RequestMethod.POST)
-    public String editerPOST(@ModelAttribute("genre") Film film){
+    public String editerPOST(@ModelAttribute("genre") Film film) {
         crud.save(film);
         return "redirect:/liste_film";
     }
+
     @RequestMapping(value = "/add_film", method = RequestMethod.POST)
-    public String ajouterPOST(@ModelAttribute("film") Film film){
+    public String ajouterPOST(@ModelAttribute("film") Film film) {
         crud.save(film);
         return "redirect:/liste_film";
     }
+
     @RequestMapping(value = "/ajouter_film", method = RequestMethod.GET)
-    public String ajouter(){
-        
-        
+    public String ajouter() {
         return "ajouter_film.jsp";
     }
 }
